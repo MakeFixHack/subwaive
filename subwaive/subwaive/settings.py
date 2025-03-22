dde8b1cd0ebe110052259696194e77a34be6ea55
dterkanian <dave.terkanian@gmail.com>
2025-03-22 16:10:00 -0400

    Set default login page

--- subwaive/subwaive/settings.py
+++ subwaive/subwaive/settings.py
@@ -132,3 +132,5 @@ STATIC_ROOT = BASE_DIR / 'subwaive/static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/admin/login/'
