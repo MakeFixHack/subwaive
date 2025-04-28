da788a1484c388ad4d3782aed20fd8fe453f63bd
dterkanian <dave.terkanian@gmail.com>
2025-04-28 12:31:13 -0400

    More rm for Event migration

--- subwaive/subwaive/admin.py
+++ subwaive/subwaive/admin.py
@@ -52,7 +52,7 @@ class PersonEmail_Admin(admin.ModelAdmin):
 admin.site.register(PersonEmail, PersonEmail_Admin)
 
 class PersonEvent_Admin(admin.ModelAdmin):
-    list_display = ('person', 'event',)
+    list_display = ('person', )
 admin.site.register(PersonEvent, PersonEvent_Admin)
 
 class PersonStripe_Admin(admin.ModelAdmin):
