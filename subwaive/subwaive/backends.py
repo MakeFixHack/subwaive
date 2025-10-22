440a9ce4b6612069d0ab8a250a2db07ba67519c7
dterkanian <dave.terkanian@gmail.com>
2025-10-22 09:13:21 -0400

    Set group permissions from ENV file

--- subwaive/subwaive/backends.py
+++ subwaive/subwaive/backends.py
@@ -84,3 +84,27 @@ class AADB2CAuthenticationBackend(OIDCAuthenticationBackend):
         user.is_staff = True
         user.save()
         return user
+
+    def set_group_permissions_from_env(group):
+        # print(f'set_group_permissions_from_env::group.name: {group.name}')
+        group_perm_list = os.environ.get(f'DJANGO_GROUP_PERMISSION_{group.name.upper().replace("-","_")}','').split(',')
+        # print(f'set_group_permissions_from_env::group_perm_list: {group_perm_list}')
+        
+        if group_perm_list == ['*']: # shorthand for admin users
+            # print("set_group_permissions_from_env using all permissions")
+            group_perm_list = [x.codename for x in Permission.objects.all()]
+        # else:
+        #     print("set_group_permissions_from_env using subset of permissions")
+
+        for p in group_perm_list:
+            # print(f'set_group_permissions_from_env::p: {p}')
+            perm_qs = Permission.objects.filter(codename=p)
+            if perm_qs:
+                perm = perm_qs.first()
+                # print(perm)
+                if not perm in group.permissions.all():
+                    group.permissions.add(perm)
+                    Log.new(logging_level=logging.CRITICAL, description='Added permission to group', json={'group': group.name, 'permission': p})
+            else:
+                Log.new(logging_level=logging.CRITICAL, description='Failed to add permission to group', json={'group': group.name, 'permission': p})
+
