8af08199423a0f588891c7bf97bfce079f9cb812
dterkanian <dave.terkanian@gmail.com>
2025-10-24 13:29:39 -0400

    Separate group creation/membership from permissions changes

--- subwaive/subwaive/backends.py
+++ subwaive/subwaive/backends.py
@@ -1,24 +1,17 @@
-import logging
-import os
-
-from django.contrib.auth.models import Permission, User, Group
-    
-from subwaive.models import Log
+from django.contrib.auth.models import Group
 
 # Classes to override default OIDCAuthenticationBackend (Keycloak authentication)
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 
-DJANGO_GROUP_STAFF_PERMISSION = os.environ.get("DJANGO_GROUP_STAFF_PERMISSION")
-
 class AADB2CAuthenticationBackend(OIDCAuthenticationBackend):
-
     def create_user(self, claims):
         """ Overrides Authentication Backend so that Django users are
             created with the keycloak preferred_username.
             If nothing found matching the email, then try the username.
         """
-        # print(f"claims::create_user: {claims}")
-        # print(super(OIDCAuthenticationBackend, self).__dir__())
+        print(f"create_user::claims: {claims}")
+        print(f"create_user::claims.get('groups',[]): {claims.get('groups',[])}")
+
         user = super().create_user(claims)
         # Keycloak field names
         user.first_name = claims['given_name']
@@ -37,9 +30,8 @@ class AADB2CAuthenticationBackend(OIDCAuthenticationBackend):
         # add new user to groups defined by OIDC provider
         for r in keycloak_roles:
             group,was_created = Group.objects.get_or_create(name=r)
+            print(f"create_user: Group '{group}' was created? {was_created}")
             group.user_set.add(user)
-
-            AADB2CAuthenticationBackend.set_group_permissions_from_env(group)
 
         return user
 
@@ -64,35 +56,17 @@ class AADB2CAuthenticationBackend(OIDCAuthenticationBackend):
 
 
     def update_user(self, user, claims):
-        # print(f"claims::update_user: {claims}")
+        #!!! when does this run?
+        #!!! update group membership
+
+        print(f"update_user::claims: {claims}")
         user.first_name = claims['given_name']
         user.last_name = claims['family_name']
         user.email = claims['email']
         user.username = claims['preferred_username']
+        #!!! condition on being a member of a group
         user.is_staff = True
+        #else: user.is_staff = False
         user.save()
         return user
-
-    def set_group_permissions_from_env(group):
-        # print(f'set_group_permissions_from_env::group.name: {group.name}')
-        group_perm_list = os.environ.get(f'DJANGO_GROUP_PERMISSION_{group.name.upper().replace("-","_")}','').split(',')
-        # print(f'set_group_permissions_from_env::group_perm_list: {group_perm_list}')
-        
-        if group_perm_list == ['*']: # shorthand for admin users
-            # print("set_group_permissions_from_env using all permissions")
-            group_perm_list = [x.codename for x in Permission.objects.all()]
-        # else:
-        #     print("set_group_permissions_from_env using subset of permissions")
-
-        for p in group_perm_list:
-            # print(f'set_group_permissions_from_env::p: {p}')
-            perm_qs = Permission.objects.filter(codename=p)
-            if perm_qs:
-                perm = perm_qs.first()
-                # print(perm)
-                if not perm in group.permissions.all():
-                    group.permissions.add(perm)
-                    Log.new(logging_level=logging.CRITICAL, description='Added permission to group', json={'group': group.name, 'permission': p})
-            else:
-                Log.new(logging_level=logging.CRITICAL, description='Failed to add permission to group', json={'group': group.name, 'permission': p})
-
+    
